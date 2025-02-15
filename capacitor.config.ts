9e1408a8142e01087dc367116ad0568d18170f41
darthleonard <leolinuxmx@gmail.com>
2025-02-14 22:53:01 -0600

    project created

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.pegaso',
+  appName: 'pegaso',
+  webDir: 'www'
+};
+
+export default config;
