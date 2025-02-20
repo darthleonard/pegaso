e4e48d76ead83b7df8723258461dc14698dcf65f
darthleonard <leolinuxmx@gmail.com>
2025-02-19 22:23:05 -0600

    app-header created

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { AppBillIconComponent } from './app-bill-icon/app-bill-icon.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
-  declarations: [AppBillIconComponent],
+  declarations: [AppBillIconComponent, AppHeaderComponent],
   imports: [CommonModule, IonicModule.forRoot()],
-  exports: [AppBillIconComponent],
+  exports: [AppBillIconComponent, AppHeaderComponent],
 })
 export class ComponentsModule {}
