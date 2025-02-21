d1769c7485f815577ccc40dc3a096edfc8a42658
darthleonard <leolinuxmx@gmail.com>
2025-02-20 23:15:58 -0600

    update colors, size and tooltip

--- src/app/components/app-bill-icon/app-bill-icon.component.ts
+++ src/app/components/app-bill-icon/app-bill-icon.component.ts
@@ -3,12 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-app-bill-icon',
   templateUrl: './app-bill-icon.component.html',
+  styles: [
+    `
+      .tooltip {
+        --width: auto;
+      }
+    `,
+  ],
   standalone: false,
 })
 export class AppBillIconComponent {
+  constructor() {}
 
-  constructor() { }
-
-  @Input() name = "logo-ionic";
+  @Input() name = 'logo-ionic';
   @Input() value: number | undefined;
 }
