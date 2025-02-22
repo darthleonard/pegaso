14b18d2aac0d1e11d10c71d884b127292b75a407
darthleonard <leolinuxmx@gmail.com>
2025-02-22 16:43:20 -0600

    main menu added

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { AppBillIconComponent } from './app-bill-icon/app-bill-icon.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { DateMonthInputComponent } from './date-month-input/date-month-input.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
-  declarations: [AppBillIconComponent, AppHeaderComponent, DateMonthInputComponent],
-  imports: [CommonModule, IonicModule.forRoot()],
-  exports: [AppBillIconComponent, AppHeaderComponent, DateMonthInputComponent],
+  declarations: [
+    MainMenuComponent,
+    AppBillIconComponent,
+    AppHeaderComponent,
+    DateMonthInputComponent,
+  ],
+  imports: [CommonModule, IonicModule.forRoot(), RouterModule],
+  exports: [
+    MainMenuComponent,
+    AppBillIconComponent,
+    AppHeaderComponent,
+    DateMonthInputComponent,
+  ],
   providers: [DatePipe],
 })
 export class ComponentsModule {}
