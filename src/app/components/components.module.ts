d89b35d108940f458a0e866cdc6dc367c96bc3fa
darthleonard <leolinuxmx@gmail.com>
2025-02-22 00:11:12 -0600

    input for month-year created

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { AppBillIconComponent } from './app-bill-icon/app-bill-icon.component';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { DateMonthInputComponent } from './date-month-input/date-month-input.component';
 
 @NgModule({
-  declarations: [AppBillIconComponent, AppHeaderComponent],
+  declarations: [AppBillIconComponent, AppHeaderComponent, DateMonthInputComponent],
   imports: [CommonModule, IonicModule.forRoot()],
-  exports: [AppBillIconComponent, AppHeaderComponent],
+  exports: [AppBillIconComponent, AppHeaderComponent, DateMonthInputComponent],
+  providers: [DatePipe],
 })
 export class ComponentsModule {}
