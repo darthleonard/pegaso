4d29abe9343f6c254e54b7900e2e6c4a8af3b778
darthleonard <leolinuxmx@gmail.com>
2025-03-08 20:26:34 -0600

    improve performance

--- api/index.php
+++ api/index.php
@@ -20,41 +20,44 @@ if ($apiKey !== 'MY_SECRET_API_KEY') {
 require_once 'core/database.php';
 require_once 'core/object-mapper.php';
 require_once 'core/router.php';
-require_once 'controllers/bill-controller.php';
-require_once 'controllers/fuel-controller.php';
-require_once 'controllers/shoppings-controller.php';
 
 $database = new DB();
 $db = $database->getConnection();
-
 $router = new Router();
 
-$billController = new BillController($db);
-$router->add('GET', '/api/index.php/bills', [$billController, 'getAllBills']);
-$router->add('GET', '/api/index.php/bills/(:id)', [$billController, 'getBillById']);
-$router->add('POST', '/api/index.php/bills', [$billController, 'createBill']);
-$router->add('PUT', '/api/index.php/bills', [$billController, 'updateBill']);
-$router->add('DELETE', '/api/index.php/bills', [$billController, 'deleteBill']);
-$router->add('GET', '/api/index.php/bills/filter', [$billController, 'getBillsByDate']);
-
-$fuelController = new FuelController($db);
-$router->add('GET', '/api/index.php/fuel', [$fuelController, 'getAllFuels']);
-$router->add('GET', '/api/index.php/fuel/(:id)', [$fuelController, 'getFuelById']);
-$router->add('POST', '/api/index.php/fuel', [$fuelController, 'createFuel']);
-$router->add('PUT', '/api/index.php/fuel', [$fuelController, 'updateFuel']);
-$router->add('DELETE', '/api/index.php/fuel', [$fuelController, 'deleteFuel']);
-$router->add('GET', '/api/index.php/fuel/filter', [$fuelController, 'getFuelsByDate']);
-
-$shoppingsController = new ShoppingsController($db);
-$router->add('GET', '/api/index.php/shoppings', [$shoppingsController, 'getAllShoppings']);
-$router->add('POST', '/api/index.php/shoppings', [$shoppingsController, 'saveShopping']);
-$router->add('PUT', '/api/index.php/shoppings', [$shoppingsController, 'saveShopping']);
-
-// to add user controller
-// $userController = new UserController($db);
-// $router->add('GET', '/users', [$userController, 'getAllUsers']);
-// $router->add('POST', '/users', [$userController, 'createUser']);
-// $router->add('GET', '/users/id', [$userController, 'getUserById']);
+$endpoint = explode('/', $_SERVER['REQUEST_URI'])[3];
+switch($endpoint) {
+    case 'bills':
+        require_once 'controllers/bill-controller.php';
+        $billController = new BillController($db);
+        $router->add('GET', '/api/index.php/bills', [$billController, 'getAllBills']);
+        $router->add('GET', '/api/index.php/bills/(:id)', [$billController, 'getBillById']);
+        $router->add('POST', '/api/index.php/bills', [$billController, 'createBill']);
+        $router->add('PUT', '/api/index.php/bills', [$billController, 'updateBill']);
+        $router->add('DELETE', '/api/index.php/bills', [$billController, 'deleteBill']);
+        $router->add('GET', '/api/index.php/bills/filter', [$billController, 'getBillsByDate']);
+        break;
+    case 'fuel':
+        require_once 'controllers/fuel-controller.php';
+        $fuelController = new FuelController($db);
+        $router->add('GET', '/api/index.php/fuel', [$fuelController, 'getAllFuels']);
+        $router->add('GET', '/api/index.php/fuel/(:id)', [$fuelController, 'getFuelById']);
+        $router->add('POST', '/api/index.php/fuel', [$fuelController, 'createFuel']);
+        $router->add('PUT', '/api/index.php/fuel', [$fuelController, 'updateFuel']);
+        $router->add('DELETE', '/api/index.php/fuel', [$fuelController, 'deleteFuel']);
+        $router->add('GET', '/api/index.php/fuel/filter', [$fuelController, 'getFuelsByDate']);
+        break;
+    case 'shoppings':
+        require_once 'controllers/shoppings-controller.php';
+        $shoppingsController = new ShoppingsController($db);
+        $router->add('GET', '/api/index.php/shoppings', [$shoppingsController, 'getAllShoppings']);
+        $router->add('POST', '/api/index.php/shoppings', [$shoppingsController, 'saveShopping']);
+        $router->add('PUT', '/api/index.php/shoppings', [$shoppingsController, 'saveShopping']);
+        break;
+    default:
+        http_response_code(404);
+        die(json_encode(["message" => "Endpoint not found"]));
+}
 
 try {
     $router->dispatch();
